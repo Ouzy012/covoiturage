687fb2c65c3078606927965f0733f202c95b8e23
Ouzy012 <ousmane.ndiaye8518@gmail.com>
2020-09-01 16:05:13 +0000

    add

--- app/models.py
+++ app/models.py
@@ -4,3 +4,8 @@ class Personne(models.Model):
     personne_id = models.AutoField(primary_key=True)
     prenom = models.CharField(max_length=50)
     nom = models.CharField(max_length=30)
+
+class Conducteur(models.Model):
+    
+    personne_id = models.ForeignKey(Personne, on_delete=models.CASCADE)
+    nbre_place = models.IntegerField()
